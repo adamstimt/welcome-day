605dd9d11996e8effcc1a3d51e128b68521fc60a
adem <adamtimtaoucine@gmail.com>
2025-11-04 12:51:55 +0100

    fix: correct tailwind content pathss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-content: [
-  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",
-],
- theme: {
-  extend: {
-    keyframes: {
-      'fade-down': {
-        '0%': { opacity: '0', transform: 'translateY(-40px)' },
-        '100%': { opacity: '1', transform: 'translateY(0)' },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
+  theme: {
+    extend: {
+      keyframes: {
+        "fade-down": {
+          "0%": { opacity: "0", transform: "translateY(-40px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        "fade-up": {
+          "0%": { opacity: "0", transform: "translateY(40px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        pop: {
+          "0%": { opacity: "0", transform: "scale(0.8)" },
+          "100%": { opacity: "1", transform: "scale(1)" },
+        },
+        "zoom-slow": {
+          "0%": { transform: "scale(1)" },
+          "100%": { transform: "scale(1.1)" },
+        },
       },
-      'fade-up': {
-        '0%': { opacity: '0', transform: 'translateY(40px)' },
-        '100%': { opacity: '1', transform: 'translateY(0)' },
+      animation: {
+        "fade-down": "fade-down 1s ease-out forwards",
+        "fade-up": "fade-up 1s ease-out forwards",
+        pop: "pop 0.8s ease-out forwards",
+        "zoom-slow": "zoom-slow 20s ease-in-out infinite alternate",
       },
-      'pop': {
-        '0%': { opacity: '0', transform: 'scale(0.8)' },
-        '100%': { opacity: '1', transform: 'scale(1)' },
-      },
-      'zoom-slow': {
-        '0%': { transform: 'scale(1)' },
-        '100%': { transform: 'scale(1.1)' },
-      },
-    },
-    animation: {
-      'fade-down': 'fade-down 1s ease-out forwards',
-      'fade-up': 'fade-up 1s ease-out forwards',
-      'pop': 'pop 0.8s ease-out forwards',
-      'zoom-slow': 'zoom-slow 20s ease-in-out infinite alternate',
     },
   },
-},
 
   plugins: [],
 };
